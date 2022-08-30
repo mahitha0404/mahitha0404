8dae28f99e68c5c63d2c4db508893a01247dcf58
mahitha0404 <112369553+mahitha0404@users.noreply.github.com>
2022-08-30 12:48:27 +0530

    Add files via upload

---
+++ maximum of three num.py
@@ -0,0 +1,8 @@
+def maximum(a, b, c): 
+   list = [a, b, c] 
+   return max(list) 
+# Driven code  
+x = int(input("Enter First number"))
+y = int(input("Enter Second number"))
+z = int(input("Enter Third number"))
+print("Maximum Number is ::>",maximum(x, y, z)) 
