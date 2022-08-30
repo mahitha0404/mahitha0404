0ce4827688d8f4255df9ddf4fe3e06625777323f
mahitha0404 <112369553+mahitha0404@users.noreply.github.com>
2022-08-30 10:13:32 +0530

    Add files via upload

---
+++ interest.py
@@ -0,0 +1,8 @@
+p=float(input("enter the principle amount : "))
+r=float(input("enter the rate of interest : "))
+t=float(input("enter the time in the years : " ))
+si=(p*r*t)/100
+print("principle amount : ",p)
+print("interest rate : ",r)
+print("time in years : ",t)
+print("simple interest : ",si)
