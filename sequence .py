92ecb9ebb812a3f8f3a1884d8546e95f8e2b0abb
mahitha0404 <112369553+mahitha0404@users.noreply.github.com>
2022-09-29 14:08:10 +0530

    Add files via upload

---
+++ sequence .py
@@ -0,0 +1,8 @@
+class Solution:
+    def mostWordsFound(self, sentences):
+        ans=0
+        for i in sentences:
+            temp=len(i.split(" "))
+            if temp >ans:
+                ans=temp
+        return ans
